583f193f466a5599cac4d430a3af889857dc9adf
saran05 <saranmylsamy@gmail.com>
2016-07-16 09:31:40 +0530

    Add files via upload

---
+++ sunday.java
@@ -0,0 +1,8 @@
+String str;
+      Scanner sc=new Scanner(System.in);
+      str=sc.nextLine();
+      if(str.equalsIgnoreCase("sunday"))
+       System.out.println("true");
+      else
+        System.out.println("false");
+      
