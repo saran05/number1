11c9ee8a56bb1907bc970b18859c8c728df4e0b9
saran05 <saranmylsamy@gmail.com>
2016-07-16 10:40:25 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,10 @@
+int n,sum=0,i;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter number");
+        n=sc.nextInt();
+        for(i=0;i<=n;i++)
+        {
+           sum+=i; 
+        }
+        System.out.println("sum is"+sum);
+ 
