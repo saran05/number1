4023d4384c99a57cad99302d03a19a416c837964
saran05 <saranmylsamy@gmail.com>
2016-07-18 14:47:07 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,9 @@
+if(year%400==0)
+            System.out.println("it is a leap year");
+        else if(year%100==0)
+            System.out.println("it is not leap year");
+        else if(year%4==0)
+            System.out.println("it is leap year");
+        else 
+            System.out.println("it is not leap year");
+        
