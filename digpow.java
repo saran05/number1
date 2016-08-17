1d8a4c781c6d239017befff3d29b3cfd74ba9825
saran05 <saranmylsamy@gmail.com>
2016-08-17 18:01:27 +0530

    Add files via upload

---
+++ digpow.java
@@ -0,0 +1,18 @@
+ while(n!=0)
+        {
+            count++;
+            n=n/10;
+        }
+        while(n1!=0)
+        {
+            n2=n1%10;
+            pow=1;
+            for(i=0;i<count;i++)
+            {
+              pow=pow*n2;
+              }
+            sum+=pow;
+            n1=n1/10;
+        }
+        System.out.println(""+sum);
+    
