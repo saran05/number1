d1898860f7c503358720cbcafee55839243d79d7
saran05 <saranmylsamy@gmail.com>
2016-08-20 15:40:47 +0530

    Add files via upload

---
+++ 2s.java
@@ -0,0 +1,8 @@
+ while(j!=0)
+                {
+                    n=j%10;
+                    if(n==2)
+                        c++;
+                    j=j/10;
+                    }}
+        System.out.println(""+c);
