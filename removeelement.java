97bd22442259f5085ee7bc09e6a4861d50b4ae8d
saran05 <saranmylsamy@gmail.com>
2016-08-22 15:33:35 +0530

    Add files via upload

---
+++ removeelement.java
@@ -0,0 +1,12 @@
+for(i=0;i<t;i++)
+        {
+            if(a[i]==n)
+            {
+                continue;
+            }
+            else{
+    b[k]=a[i];
+    k++;
+    }}
+        for(i=0;i<k;i++)
+            System.out.print(" "+b[i]);
