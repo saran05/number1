ce89514eef95ba7af3ac00439d7c1cc282c403e2
saran05 <saranmylsamy@gmail.com>
2016-08-22 14:24:44 +0530

    Add files via upload

---
+++ revoddstr1.java
@@ -0,0 +1,18 @@
+for(i=0;i<t;i++)
+        {
+           if(i%2==0)
+           {
+         String ss=new StringBuffer(s1[i]).reverse().toString();
+         s1[i]=ss;
+         System.out.print(""+s1[i]); 
+               if(i!=t-1)
+               System.out.print(" ");
+           }else{
+                System.out.print(""+s1[i]);
+           if(i!=t-1)
+                System.out.print(" ");
+        }
+      
+        
+        }
+    
