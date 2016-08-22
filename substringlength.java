c9e8cbdedc024c54c8af507fa1835bbcf2223eff
saran05 <saranmylsamy@gmail.com>
2016-08-22 17:30:47 +0530

    Add files via upload

---
+++ substringlength.java
@@ -0,0 +1,8 @@
+ char[] c=s.toCharArray();
+        char c1=c[0];
+        for(j=i+2;j<l-2;j++)
+        {
+           String s1="";
+           s1=s1+c1+c[j];
+            System.out.println(""+s1);
+       
