57648cd543977662693750bd15fb6bc3a710bc35
saran05 <saranmylsamy@gmail.com>
2016-08-22 12:39:38 +0530

    Add files via upload

---
+++ arrayimm.java
@@ -0,0 +1,21 @@
+for(i=0;i<5;i++)
+        {if(i==4)
+        {
+            if(a[i]>a[0])
+                    {
+                        System.out.println(""+a[0]);
+                    }else
+                System.out.println("-1");
+        }else{
+            if(a[i]>a[i+1])
+            {
+                System.out.println(""+a[i+1]);
+            }
+            else
+            {
+                System.out.println("-1");
+            }
+        }
+        
+    }
+    
