e35788b420cefa10202b79a658e56521e5ff0c67
saran05 <saranmylsamy@gmail.com>
2016-08-27 11:47:34 +0530

    Add files via upload

---
+++ panagram.java
@@ -0,0 +1,10 @@
+ for(i=0;i<c1;i++)
+        {
+            if(64<=c[i]&&c[i]<=90||97<=c[i]&&c[i]<=122) {
+                count++;
+             }}
+        if(count>=26)
+        
+            System.out.println("panagram");
+        else
+            System.out.println("not panagram");
